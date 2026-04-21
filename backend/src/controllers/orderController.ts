b44d67b8003efd682b28e9aeecb05a1498322bce
Nathaniel Escano <205729250+Natex15@users.noreply.github.com>
2026-04-21 13:12:18 +0800

    fix: revised order updates logic

--- backend/src/controllers/orderController.ts
+++ backend/src/controllers/orderController.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from "express";
 import Order from "../models/orderModel";
-import { IUserSafe } from "../models/userModel";
 import { Types } from "mongoose";
+import "../types/express.d";
 
-interface AuthRequest extends Request {
-  user?: IUserSafe;
-}
-
-export const getOrders = async (req: AuthRequest, res: Response) => {
+export const getOrders = async (req: Request, res: Response) => {
   try {
     const user = req.user;
     if (!user) {
@@ -21,22 +17,25 @@ export const getOrders = async (req: AuthRequest, res: Response) => {
   }
 };
 
-export const createOrder = async (req: AuthRequest, res: Response) => {
+export const createOrder = async (req: Request, res: Response) => {
   try {
-    const { transactionId, productId, orderQuantity, email, dateOrdered } = req.body as {
+    const user = req.user;
+    if (!user) {
+      res.status(401).json({ message: "Not authorized" });
+      return;
+    }
+    const { transactionId, productId, orderQuantity } = req.body as {
       transactionId: string;
       productId: Types.ObjectId;
       orderQuantity: number;
-      email: string;
-      dateOrdered: Date;
     };
     const newOrder = await Order.create({
       transactionId,
       productId,
       orderQuantity,
       orderStatus: 0,
-      email,
-      dateOrdered,
+      email: user.email,
+      dateOrdered: new Date(),
     });
     res.status(200).json(newOrder);
   } catch (_error) {
@@ -44,21 +43,21 @@ export const createOrder = async (req: AuthRequest, res: Response) => {
   }
 };
 
-export const cancelOrder = async (req: AuthRequest, res: Response) => {
+export const cancelOrder = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const user = req.user;
     if (!user) {
       return res.status(401).json({ message: "Not authorized" });
     }
+
     const cancelledOrders = await Order.updateMany(
-      { transactionId: id, email: user.email },
+      { transactionId: id, email: user.email, orderStatus: 0 },
       { $set: { orderStatus: 2 } }
     );
 
     if (cancelledOrders.matchedCount === 0) {
       return res.status(404).json({ message: "No orders found to cancel." });
-      return;
     }
     return res.status(200).json({
       message: "Successfully cancelled order.",
